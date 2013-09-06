92b7c6d98dbe1de33c1e887386c8101a7a273593
Margo <margo@margonline.co.uk>
2013-09-06 17:18:57 +0100

    added context to plane_spec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,34 +8,36 @@ require 'plane'
     plane.take_off?(airport).should be_true
   end
 
-  it 'does not take_off if permmission not given by airport' do
-    airport = double :airport, {:permission_given_to_take_off? => false}
-    plane.take_off?(airport).should be_false
-  end
-
   it 'can land' do
     airport = double :airport, {:permission_given_to_land? => true}
     plane.land?(airport).should be_true
   end
 
-  it 'does not land if permission not given by airport' do
-    airport = double :airport, {:permission_given_to_land? => false}
-    plane.land?(airport).should be_false
+  context 'if permmission not given by airport' do
+    it 'does not take_off' do
+      airport = double :airport, {:permission_given_to_take_off? => false}
+      plane.take_off?(airport).should be_false
+    end
+
+    it 'does not land' do
+      airport = double :airport, {:permission_given_to_land? => false}
+      plane.land?(airport).should be_false
+    end
   end
 
-  it 'calls the airport for permission to take_off' do
-    airport = double :airport
-    airport.should_receive(:permission_given_to_take_off?)
+  context 'calls the airport for permission to' do
+    it 'take_off' do
+      airport = double :airport
+      airport.should_receive(:permission_given_to_take_off?)
+      plane.take_off?(airport)
+    end
 
-    plane.take_off?(airport)
+    it 'land' do
+      airport = double :airport 
+      airport.should_receive(:permission_given_to_land?)
+      plane.land?(airport)
+    end
   end
-
-  it 'calls airport for permission to land' do
-    airport = double :airport 
-    airport.should_receive(:permission_given_to_land?)
-    plane.land?(airport)
-  end
-
   
 end
 
